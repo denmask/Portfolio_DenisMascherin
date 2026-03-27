e143bbcf75458810b0956722e5540d70a5aaf1bb
Denis Mascherin <denis.mascherin.24@stud.itsaltoadriatico.it>
2026-03-27 14:50:33 +0100

    TOLTO COMMENTO

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const observer = new IntersectionObserver(function(entries) {
   entries.forEach(function(entry) {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
-      observer.unobserve(entry.target); // smette di osservare dopo la prima animazione
+      observer.unobserve(entry.target); 
     }
   });
 }, { threshold: 0.12 });
